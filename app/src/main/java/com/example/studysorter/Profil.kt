5a97b04725f5dfcda16af3d0d8392664db17041c
kasia <ul0275820@edu.uni.lodz.pl>
2024-03-24 22:43:05 +0100

    zmiana avatara

--- app/src/main/java/com/example/studysorter/Profil.kt
+++ app/src/main/java/com/example/studysorter/Profil.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageButton
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -16,6 +18,7 @@ class Profil : AppCompatActivity() {
     private val chmura = FirebaseFirestore.getInstance()
     private lateinit var userEmailTextView: TextView
     private lateinit var storageReference: StorageReference
+    private var selectedImageUri: Uri? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,6 +40,11 @@ class Profil : AppCompatActivity() {
         } else {
             userEmailTextView.text = "Brak zalogowanego użytkownika"
         }
+
+        val saveButton: Button = findViewById(R.id.saveButton)
+        saveButton.setOnClickListener {
+            saveImageUriToFirebase()
+        }
     }
 
     private fun chooseImageFromGallery() {
@@ -51,38 +59,43 @@ class Profil : AppCompatActivity() {
         super.onActivityResult(requestCode, resultCode, data)
 
         if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.data != null) {
-            val imageUri = data.data
-            if (imageUri != null) {
-                uploadImageToFirebaseStorage(imageUri)
+            selectedImageUri = data.data
+            if (selectedImageUri != null) {
+                // Set the selected image to the avatar ImageView
+                findViewById<ImageView>(R.id.avatar).setImageURI(selectedImageUri)
             } else {
                 Toast.makeText(this, "Nie udało się uzyskać adresu URI pliku", Toast.LENGTH_SHORT).show()
             }
         }
     }
-    private fun uploadImageToFirebaseStorage(imageUri: Uri) {
+    private fun saveImageUriToFirebase() {
         val currentUser = FirebaseAuth.getInstance().currentUser
-        if (currentUser != null) {
+        if (currentUser != null && selectedImageUri != null) {
             val fileName = "avatar_${currentUser.uid}.jpg"
             val fileRef = storageReference.child("avatars").child(fileName)
-            val uploadTask = fileRef.putFile(imageUri)
+            val uploadTask = fileRef.putFile(selectedImageUri!!)
             uploadTask.addOnSuccessListener {
 
-                //pobierz adres URL przesłanego zdjęcia i zapisz go w bazie danych Firestore
+                // Get the download URL of the uploaded image
                 fileRef.downloadUrl.addOnSuccessListener { uri ->
-                    val userDocRef = chmura.collection("users").document(currentUser.uid)
-                    userDocRef.update("avatarUrl", uri.toString())
-                        .addOnSuccessListener {
-                            Toast.makeText(this, "Zmiana avatara zakończona pomyślnie", Toast.LENGTH_SHORT).show()
-                        }
-                        .addOnFailureListener { e ->
-                            Toast.makeText(this, "Wystąpił błąd podczas zmiany avatara: ${e.message}", Toast.LENGTH_SHORT).show()
-                        }
+                    val avatarImageButton: ImageView = findViewById(R.id.avatar)
+
+                    // Set the selected image to the avatar ImageButton
+                    avatarImageButton.setImageURI(selectedImageUri)
+
+                    Toast.makeText(this, "Zmiana avatara zakończona pomyślnie", Toast.LENGTH_SHORT).show()
+                }.addOnFailureListener { exception ->
+                    Toast.makeText(this, "Wystąpił błąd podczas pobierania adresu URL obrazu: ${exception.message}", Toast.LENGTH_SHORT).show()
                 }
             }.addOnFailureListener { exception ->
                 Toast.makeText(this, "Wystąpił błąd podczas przesyłania zdjęcia: ${exception.message}", Toast.LENGTH_SHORT).show()
             }
+        } else {
+            Toast.makeText(this, "Brak zalogowanego użytkownika lub nie wybrano zdjęcia", Toast.LENGTH_SHORT).show()
         }
     }
+
+
 
 
 }

b6c143d044aaf175428acc553a5b83f581d3654f
Angelika <ul0275849@edu.uni.lodz.pl>
2024-05-07 05:44:23 +0200

    dodanie przejścia do przedmiotu

--- app/src/main/java/com/example/studysorter/navigation/Screens.kt
+++ app/src/main/java/com/example/studysorter/navigation/Screens.kt
@@ -1,10 +1,12 @@
 package com.example.studysorter.navigation
 
 
+
 sealed class Screens(var route: String) {
 
     data object  Home : Screens("home")
     data object  Profile : Screens("profile")
     data object  Przedmioty : Screens("przedmioty")
     data object  Ulubione : Screens("ulubione")
+    data class Detail(val subjectId: String) : Screens("${Screens.Przedmioty.route}/{subjectId}")
 }
